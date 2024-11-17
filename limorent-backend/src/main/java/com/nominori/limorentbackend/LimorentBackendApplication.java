6af80eb7cf331f21fcb8219718d2f2196d8312ed
nominori <antonshimchico37@gmail.com>
2024-11-17 16:54:47 +0100

    feat(backend): initialize spring boot backend project

---
+++ limorent-backend/src/main/java/com/nominori/limorentbackend/LimorentBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nominori.limorentbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimorentBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LimorentBackendApplication.class, args);
+    }
+
+}
